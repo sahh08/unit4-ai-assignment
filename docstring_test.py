0a4b2a900002c7ec3205c8a53126d2479b246967
Sahana <sahanagowda.hyb@gmail.com>
2025-11-20 19:48:37 +0530

    Add docstrings using Copilot

---
+++ docstring_test.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+"""Generate docstring"""
+def add(a, b):
+    """
+    Adds two numbers.
+
+    Parameters:
+    a (int, float): The first number.
+    b (int, float): The second number.
+
+    Returns:
+    int, float: The sum of the two numbers.
+    """
+    return a + b
